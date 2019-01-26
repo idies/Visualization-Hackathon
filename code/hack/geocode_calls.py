275b34700fe1eed395e658e0fa64fbc57b296733
Darius Irani <dirani2@jhu.edu>
2019-01-26 18:30:56 -0500

    added code to geocode coordinates from 911 call logs and visualize using altair, converted from jupyter notebook

--- code/hack/geocode_calls.py
+++ code/hack/geocode_calls.py
@@ -1,30 +1,12 @@
 import pandas as pd
 import geopandas as gpd
 import os
+import sys
 import numpy as np
 import pickle
-import ast
 from shapely.geometry import Point
 
-fname = os.path.join('data/pickles', 'baltimore.pkl')
-gdf = pickle.load(open(fname, 'rb'))
-df = pd.read_csv('data/911_calls_for_service.csv')
-df.drop(columns='Unnamed: 0', inplace=True)
-latlon = df['location'].str.strip('()').str.split(',', expand=True).rename(columns={0:'lat', 1:'lon'}) 
-df = pd.concat([df, latlon], axis=1)
-geocoded = pd.DataFrame({'lat':[""],'lon':[""], 'congressional_district':[""],'county':[""],
-                           'legislative_district':[""], 'neighborhood':[""],
-                           'zipcode':[""]})
-for i in range(len(df.iloc[:1000])):
-    geocoded = geocoded.append(geocode_point(df.loc[i,'lat'], df.loc[i,'lon']), sort=False)
-geocoded = geocoded.iloc[1:]
-geocoded = geocoded.reset_index()
-geocoded['location'] = geocoded.apply(lambda x: str('(' + x.lat + ', ' + x.lon + ')'), axis=1)
-geocoded.drop(columns=['lat', 'lon'], inplace=True)
-geocoded = geocoded[['location', 'congressional_district', 'county', 'legislative_district',
-                     'neighborhood', 'zipcode']]
-
-def geocode(gdf, directory='data/pickles', pickle_name='baltimore.pkl'):
+def geocode(gdf, directory, pickle_name='baltimore.pkl'):
     reference = pickle.load(open(os.path.join(directory, pickle_name), 'rb'))
     try:
         df = gpd.sjoin(gdf, reference, how = 'left')
@@ -36,8 +18,8 @@ def geocode(gdf, directory='data/pickles', pickle_name='baltimore.pkl'):
         df.drop(columns=['level_0'], inplace=True)
         df.columns.name = None
     except:
-        df = pd.DataFrame({'lat':[gdf.lat.item()],'lon':[gdf.lon.item()], 'congressional_district':[np.nan], 'county':[np.nan],
-                           'legislative_district':[np.nan], 'neighborhood':[np.nan],
+        df = pd.DataFrame({'lat':[gdf.lat.item()],'lon':[gdf.lon.item()], 'census_tract':[np.nan], 'congressional_district':[np.nan],
+                           'county':[np.nan], 'legislative_district':[np.nan], 'neighborhood':[np.nan],
                            'zipcode':[np.nan]})
     return df
 
@@ -46,14 +28,60 @@ def geocode_point(left, right):
         point = pd.DataFrame(data={'lat': [left], 'lon': [right], 'geometry':
             [Point((float(right), float(left)))]})
     except:
-        return pd.DataFrame({'lat':[left],'lon':[right],
+        return pd.DataFrame({'lat':[left],'lon':[right], 'census_tract':[np.nan],
                             'congressional_district':[np.nan], 'county':[np.nan],
                            'legislative_district':[np.nan], 'neighborhood':[np.nan],
                            'zipcode':[np.nan]})
     point = point[['lat', 'lon', 'geometry']]
     point = gpd.GeoDataFrame(point, geometry='geometry')
     point.crs = {'init' :'epsg:4326'}
-    return geocode(point)
+    return geocode(point, os.path.join(os.pardir, os.pardir, 'data/pickles'))
 
+def geocode_df(df):
+    df['geometry'] = df.apply(lambda x: Point((float(x[1]), float(x[0]))), axis=1)
+    df = gpd.GeoDataFrame(df, geometry='geometry')
+    df.crs = {'init' :'epsg:4326'}
+    return geocode(df, os.path.join(os.pardir, os.pardir, 'data/pickles'))
 
-def main()
+def _chunker(df, chunks):
+        size = len(df) // chunks
+        return (df[pos:pos + size] for pos in range(0, len(df), size))
+    
+def process_chunk(df):
+        temp = []
+        for chunk in _chunker(df, 100):
+            coords = chunk.loc[:, ('lat', 'lon')]
+            data = geocode_df(coords)
+            temp.append(pd.merge(chunk, data, left_on=['lat', 'lon'],
+                right_on=['lat', 'lon'], how='left').drop_duplicates())
+        df = pd.concat(temp, sort=False).reset_index()
+        df['location'] = df.apply(lambda x: str('(' + x.lat + ', ' + x.lon + ')'), axis=1)
+        df.drop(columns=['lat', 'lon', 'index'], inplace=True)
+        return df[['recordId','callDateTime','priority','district','description','callNumber',
+                   'incidentLocation','location','census_tract','congressional_district','county',
+                   'legislative_district','neighborhood','zipcode']]
+
+def main(dataset='911_Police_Calls_for_Service.csv', pickle_name='baltimore.pkl', directory='data/baltimore_crime', pickle_dir='data/pickles'):
+    fname = os.path.join(pickle_dir, pickle_name)
+    gdf = pickle.load(open(fname, 'rb'))
+    df = pd.read_csv(os.path.join(directory, dataset)
+    df.location = "(" + df.location.str.split('(').str[1]
+    df = df[pd.notnull(df['location'])].reset_index(drop=True)
+    latlon = df['location'].str.strip('()').str.split(',', expand=True).rename(columns={0:'lat', 1:'lon'}) 
+    df = pd.concat([df, latlon], axis=1)
+    geocoded = process_chunk(df)
+    temp.to_csv(os.path.join(directory, '911_geocoded.csv'), index=False)
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        main()
+    else if len(sys.argv) == 2:
+        main(dataset=sys.argv[1])
+    else if len(sys.argv) == 3:
+        main(dataset=sys.argv[1], pickle_name=sys.argv[2])
+    else if len(sys.argv) == 4:
+        main(dataset=sys.argv[1], pickle_name=sys.argv[2], directory=sys.argv[3])
+    else if len(sys.argv) == 5:
+        main(dataset=sys.argv[1], pickle_name=sys.argv[2], directory=sys.argv[3], pickle_dir=sys.argv[4])
+    else:
+        print("invalid number of command line arguments")
--- code/hack/visualize_map.py
+++ code/hack/visualize_map.py
@@ -15,85 +15,81 @@ import pandas as pd
 import geopandas as gpd
 import json
 import shapely.wkt
-
-colors = 9
-cmap = 'Blues'
-figsize = (16, 10)
+import pickle
+import numpy as np
 
 
-def get_blockgroup_geometries(pickle_dir, pickle_name):
+def load_pickle(pickle_dir, pickle_name):
     """
     Loads SeattleCensusBlockGroups geometries as geodataframe from pickle file.
     Inputs: pickle
     Outputs: geodataframe, pickle if it does not exist
     """
-    fname = os.path.join(pickle_dir, pickle_name)
-    gdf = pickle.load(open(fname, 'rb'))
-    return gdf
+    return pickle.load(open(os.path.join(pickle_dir, pickle_name), 'rb'))
 
 
-def load_choropleth_attribute(file_name, processed_dir):
-    """
-    Loads data for choropleth into dataframe from csv file.
-    Inputs: csv file name for data desired to be choropleth
-    Outputs: dataframe
-    """
-    df = pd.read_csv(os.path.join(processed_dir, file_name), dtype={'key': str})
-    df.key = df.key.apply(lambda x: x.rstrip('.0'))
-    return df
-
-
-def merge_data(processed_dir, df=None, file_name=None):
+def merge_data(attribute_column, geography, chloropleth, pickle_dir):
     """
     Merges geometry geodataframe with chloropleth attribute data.
     Inputs: dataframe or csv file name for data desired to be choropleth
     Outputs: dataframe
     """
-    block_groups = get_blockgroup_geometries()
-    if df is not None:
-        attribute = df
-    else:
-        attribute = load_choropleth_attribute(file_name, processed_dir)
-    
-    return block_groups.merge(attribute, on='key', how='inner')
+    gdf = load_pickle(pickle_dir, geography)
+    chloropleth = load_pickle(pickle_dir, chloropleth)
+    chloropleth.columns = ['key', attribute_column]
+    return gdf.merge(chloropleth, on='key', how='left')
     
 
-def plot_map(attribute_column, processed_dir, df=None, file_name=None):
+def plot_map(attribute_column, geography, chloropleth, pickle_dir='data/pickles'):
     """
     Plots geodataframe using the attribute as the chloropleth
     Inputs: column to be plotted as chloropleth, filename of csv file (optional)
     Outputs: plot
     """
-    df = merge_data(df, file_name, processed_dir)
+    df = merge_data(attribute_column, geography, chloropleth, pickle_dir)
     gdf = gpd.GeoDataFrame(df, crs={'init' :'epsg:4326'}, geometry='geometry')
     gdf.plot(column=attribute_column, cmap=cmap, figsize=figsize, scheme='equal_interval',
-        k=colors, categorical=True, legend=True)
+        k=colors, categorical=False, legend=True)
     
-def prepare_for_altair(self, processed_dir, df=None, file_name=None):
-    df = self.merge_data(df, file_name, processed_dir)
+
+def prepare_for_altair(attribute_column, geography, chloropleth, pickle_dir='data/pickles'):
+    df = merge_data(attribute_column, geography, chloropleth, pickle_dir)
     gdf = gpd.GeoDataFrame(df, crs={'init' :'epsg:4326'}, geometry='geometry')
+    gdf = gdf[['key', 'category', attribute_column, 'geometry']]
     json_gdf = gdf.to_json()
     json_features = json.loads(json_gdf)
     return alt.Data(values=json_features['features'])
 
 
-def plot_map_altair(self, processed_dir, df=None, file_name=None):
-    data_geo = self.prepare_for_altair(df, file_name, processed_dir)
+def plot_map_altair(attribute_column, geography, chloropleth, pickle_dir='data/pickles'):
+    data_geo = prepare_for_altair(attribute_column, geography, chloropleth, pickle_dir)
     multi = alt.selection_multi()
-    return alt.Chart(data_geo).mark_geoshape(
+    highlight = alt.selection(type='single', on='mouseover',
+                          fields=['symbol'], nearest=True)
+    baltimore = alt.Chart(data_geo).mark_geoshape(
         fill='lightgray',
         stroke='white'
     ).properties(
         projection={'type': 'mercator'},
-        width=300,
+        width=550,
         height=600,
         selection=multi
     ).encode(
-        color=alt.condition(multi, 'properties.mode_index_scaled:Q', alt.value('lightgray')),
-        tooltip=('properties.key:Q', 'properties.neighborhood_short:N',
-                 'properties.neighborhood_long:N', 'properties.seattle_city_council_district:N',
-                 'properties.urban_village:N', 'properties.zipcode:N', 'properties.mode_index:Q',
-                 'properties.driving:Q', 'properties.transit:Q', 
-                 'properties.bicycling:Q', 'properties.walking:Q')
+        color=alt.condition(multi, 'properties.' + attribute_column +':Q', alt.value('lightgray')),
+        tooltip=('properties.key:O', 'properties.' + attribute_column +':Q')
     )
+    return baltimore
 
+def main(attribute, chloropleth, geomap='baltimore.pkl', directory='data/geography'):
+    chart = plot_map_altair(attribute, geomap, chloropleth)
+    chart.save(os.path.join(directory, attribute + '.html'))
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        main(attribute=sys.argv[1], chloropleth=sys.argv[2])
+    else if len(sys.argv) == 4:
+        main(attribute=sys.argv[1], chloropleth=sys.argv[2], geomap=sys.argv[3])
+    else if len(sys.argv) == 5:
+        main(attribute=sys.argv[1], chloropleth=sys.argv[2], geomap=sys.argv[3], directory=sys.argv[4])
+    else:
+        print("invalid number of command line arguments")
